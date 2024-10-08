f58945c25b5ec478df61672dd41ae8bcb99cc7a0
magisterblaman <hannes.blaman@lbs.se>
2024-10-08 13:33:02 +0200

    XML-kommentarer

---
+++ SY22_PRRPRR02_11_XML-kommentarer/KoolKlass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY22_PRRPRR02_11_XML_kommentarer {
+	/// <summary>
+	/// 
+	/// </summary>
+	internal class KoolKlass {
+		private int koolVariabel;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int KoolEgenskap {
+			get {
+				return koolVariabel;
+			}
+			set {
+				koolVariabel = value;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="inputKoolVariabel"></param>
+		public KoolKlass(int inputKoolVariabel) {
+			// sätter ett värde på KoolEgenskap
+			KoolEgenskap = inputKoolVariabel;
+		}
+
+		/// <summary>
+		/// Multiplicerar ett tal med 69, ett med 420 och beräknar summan.
+		/// </summary>
+		/// <param name="koolParameter1">Det första talet</param>
+		/// <param name="koolParameter2">Det andra talet</param>
+		/// <returns>Summan av produkterna</returns>
+		public int KoolMetod(int koolParameter1, int koolParameter2) {
+			return 69 * koolParameter1 + 420 * koolParameter2;
+		}
+	}
+}
--- SY22_PRRPRR02_11_XML-kommentarer/Program.cs
+++ SY22_PRRPRR02_11_XML-kommentarer/Program.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace SY22_PRRPRR02_11_XML_kommentarer {
 	internal class Program {
+
 		static void Main(string[] args) {
+
+			KoolKlass kool = new KoolKlass(4327);
+
+			kool.KoolMetod(32, 12);
+
 		}
 	}
 }
